49c6fc7ed3511233a31f8426eda8818f86f3b283
hanifnaufal <hanifnaufal7557@gmail.com>
2018-03-23 08:43:52 +0700

    using stdin

--- example/Main.java
+++ example/Main.java
@@ -1,12 +1,12 @@
+import java.util.Scanner;
 public class Main {
   public static void main(String[] args) {
-    System.out.println("Hello World");
-    try {
-      // Thread.sleep(3000);
-      System.out.println("No sleep");
-    } catch (Exception e) {
-      System.out.println(":(");
+    Scanner sc = new Scanner(System.in);
+    while(sc.hasNext()) {
+      System.out.println(sc.nextLine());
     }
-    System.out.println("Hello Nif");
+    for (int i = 0;i < 5; i++) {
+      System.out.println("Hello World");
+    }
   }
 }
