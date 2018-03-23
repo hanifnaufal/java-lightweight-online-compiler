07415d38cf12f20acd20594e402b12e89682e618
hanifnaufal <hanifnaufal7557@gmail.com>
2018-03-23 10:20:34 +0700

    template done

--- bin/codeGenerator.js
+++ bin/codeGenerator.js
@@ -1,9 +1,16 @@
-var templateBegin = "public class Main {\n  public static void main(String[] args) {\n  }\n}\n";
+var templateBegin = "public class Main {\n\tpublic static void main(String[] args) {\n\t\t";
+var templateEnd = "\n\t}\n}\n";
 var CodeGenerator = {};
 CodeGenerator.generate = (input, callback) => {
-      callback({
-          code: templateBegin
-      });
+  var formattedInput = reformat(input);
+  var code = templateBegin + formattedInput + templateEnd;
+  callback({
+    code: code
+  });
 };
+
+function reformat(input) {
+  return input.replace(new RegExp("\n", 'g'), "\n\t\t");
+}
 
 module.exports = CodeGenerator;
